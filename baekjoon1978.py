d9fd873b611673b8bcbec59717c843361eb8107d
chwangmin <ckm7907cb@gmail.com>
2023-04-07 20:14:12 +0900

    beakjoon: 1978 -PTA

--- baekjoon1978.py
+++ baekjoon1978.py
@@ -1,10 +1,15 @@
 import sys
 
-num = int(sys.stdin.readline())
+re_count = int(sys.stdin.readline())
 
-list_num = list(map(int,sys.stdin.readline().split()))
+prime_nums = list()
 
-def prime_Check(n):
+def prime_list(n):
+    for i in range(2,n-1):
+        if prime_check(i):
+            prime_nums.append(i)
+
+def prime_check(n):
     i = 2
     while i * i <= n:
         if n % i == 0:
@@ -12,11 +17,14 @@ def prime_Check(n):
         i+=1
     return True
 
-count = 0
+prime_list(10000)
 
-for i in list_num:
-    if i > 1:
-        if prime_Check(i):
-            count+=1
+for i in range(re_count):
+    num = int(sys.stdin.readline())
 
-print(count)
+
+    for i in range(num // 2, 0, -1):
+        if i in prime_nums:
+            if num - i in prime_nums:
+                print(str(i) + " " + str(num-i))
+                break
