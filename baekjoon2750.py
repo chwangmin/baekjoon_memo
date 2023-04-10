44b5c154b479b6626b47846072052f2d3b26444f
chwangmin <ckm7907cb@gmail.com>
2023-04-10 15:23:07 +0900

    beakjoon: 2750

--- baekjoon2750.py
+++ baekjoon2750.py
@@ -2,10 +2,13 @@ import sys
 
 num = int(sys.stdin.readline())
 
-list_sort = []
+list_sort = [0] * 10001
 
 for i in range(num):
-    list_sort.append(int(sys.stdin.readline()))
+    list_sort[int(sys.stdin.readline())] += 1
 
-for i in sorted(list_sort):
-    print(i)
+for i in range(10001):
+    if list_sort[i] > 0:
+        while list_sort[i] > 0:
+            list_sort[i] -=1
+            print(i)
