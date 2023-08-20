a287843c9e2167f4e3f2dbea37cbeffcfec95218
chwangmin <44563065+chwangmin@users.noreply.github.com>
2023-08-20 16:02:03 +0900

    [Bronze I] Title: 히든 넘버, Time: 380 ms, Memory: 91428 KB -BaekjoonHub

--- 백준/Bronze/8595. 히든 넘버/히든 넘버.py
+++ 백준/Bronze/8595. 히든 넘버/히든 넘버.py
@@ -1,15 +1,4 @@
+import re
 input()
 s = input()
-hidden_numbers = []
-is_number = False
-num = ""
-for i in s:
-    if i not in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
-        if len(num) < 7 and len(num) != 0:
-            hidden_numbers.append(int(num))
-        num = ""
-        continue
-    num += i
-if len(num) < 7 and len(num) != 0:
-        hidden_numbers.append(int(num))
-print(sum(hidden_numbers))
+print(sum(map(int, re.findall('[0-9]+',s))))
