5a6301a449d957c4887e4341895b6521ada2f2f5
chwangmin <44563065+chwangmin@users.noreply.github.com>
2024-01-02 22:37:24 +0900

    [Bronze V] Title: A/B, Time: 220 ms, Memory: 17984 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double A = sc.nextDouble();
+        double B = sc.nextDouble();
+        
+        sc.close();
+
+        System.out.print(A/B);
+    }
+}
