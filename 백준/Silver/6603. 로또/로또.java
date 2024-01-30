7e4715752958f43e0b436ddcab6463a2c5d55a3a
chwangmin <44563065+chwangmin@users.noreply.github.com>
2024-01-30 22:54:49 +0900

    [Silver II] Title: 로또, Time: 160 ms, Memory: 14928 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	final static int LOTTO_NUM = 6;
+	static int numsLength;
+	static int[] nums;
+	static int[] answer;
+	static StringBuilder sb = new StringBuilder();
+	static boolean[] isSelected;
+
+	static void inputData(StringTokenizer st) {
+		int numsIndex = 0;
+		while (st.hasMoreTokens()) {
+			nums[numsIndex++] = Integer.parseInt(st.nextToken());
+		}
+	}
+
+	static void lottoComb(int currentIdx, int start) {
+		if (currentIdx == LOTTO_NUM) {
+			for (int i = 0; i < LOTTO_NUM; i++) {
+				sb.append(answer[i] + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for (int i = start; i < numsLength; i++) {
+			answer[currentIdx] = nums[i];
+			lottoComb(currentIdx + 1, i + 1);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+
+		while (true) {
+			st = new StringTokenizer(br.readLine());
+			numsLength = Integer.parseInt(st.nextToken());
+
+			if (numsLength == 0) {
+				break;
+			}
+
+			nums = new int[numsLength];
+			answer = new int[numsLength];
+			isSelected = new boolean[numsLength];
+
+			inputData(st);
+
+			lottoComb(0, 0);
+			
+			sb.append("\n");
+		}
+
+		System.out.println(sb);
+	}
+}
