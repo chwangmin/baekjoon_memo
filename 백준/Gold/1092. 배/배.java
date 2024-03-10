fcfe9ab8ae3e32b0abd813a71778977943095de5
chwangmin <44563065+chwangmin@users.noreply.github.com>
2024-03-10 15:25:42 +0900

    [Gold V] Title: 배, Time: 300 ms, Memory: 16352 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -1,67 +1,83 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.StringTokenizer;
-
-/**
- * 완탐 사용 x
- */
-
+import java.util.*;
+ 
+ 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st = null;
-
-    static int N, M, craneIdx, boxIdx, answer = 1;
-    static Integer[] craneWeights, boxWeights;
-
-    public static void main(String[] args) throws IOException {
-        N = Integer.parseInt(br.readLine());
-        craneWeights = new Integer[N];
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            craneWeights[i] = Integer.parseInt(st.nextToken());
-        }
-
-        Arrays.sort(craneWeights, Collections.reverseOrder());
-
-        M = Integer.parseInt(br.readLine());
-        boxWeights = new Integer[M];
-        st = new StringTokenizer(br.readLine());
-
-        ArrayList<Integer> boxWeights = new ArrayList<>();
-        for (int i = 0; i < M; i++) {
-            boxWeights.add(Integer.parseInt(st.nextToken()));
-        }
-
-        Collections.sort(boxWeights, Collections.reverseOrder());
-
-        if (craneWeights[0] < boxWeights.get(0)) {
-            System.out.println(-1);
-            return;
-        }
-
-        boxIdx = 0;
-        craneIdx = 0;
-        while (!boxWeights.isEmpty()) {
-            if (craneIdx == N || boxIdx == M){
-                craneIdx = 0;
-                boxIdx = 0;
-                answer++;
-            }
-            if (craneWeights[craneIdx] >= boxWeights.get(boxIdx)){
-                craneIdx++;
-                M--;
-                boxWeights.remove(boxIdx);
-            }
-            else {
-                boxIdx++;
-            }
-        }
-
-        System.out.println(answer);
+	
+	public static int N,M;
+	public static Integer[] crain, box;
+	public static int answer =0 ;
+	public static boolean[] visited;
+	public static int[] crain_positions;
+    public static void main(String[] args) throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	
+    	StringTokenizer st = new StringTokenizer(br.readLine());
+    	N = Integer.parseInt(st.nextToken());
+    	crain = new Integer[N];
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for(int i=0;i<N;i++) {
+    		crain[i] = Integer.parseInt(st.nextToken());
+    	}
+ 
+    	st = new StringTokenizer(br.readLine());
+    	M = Integer.parseInt(st.nextToken());
+    	box = new Integer[M];
+    	
+    	st = new StringTokenizer(br.readLine());
+    	for(int i=0;i<M;i++) {
+    		box[i] = Integer.parseInt(st.nextToken());
+    	}
+    	
+    	//내림차순으로 정렬하여야, 큰것들부터 처리합니다. (큰 크레인이 작은것들을 처리하는 일이 없어야만 합니다.)
+    	Arrays.sort(crain, Collections.reverseOrder());
+    	Arrays.sort(box, Collections.reverseOrder());
+    	
+    	
+    	if(crain[0] < box[0]) {
+    		System.out.println("-1"); 
+    		return;
+    	}
+ 
+    	crain_positions = new int[N];
+    	visited = new boolean[M];
+    	//박스를 모두 옮기기전까지 작동
+    	while(M > 0) {
+    		
+    		//각 크레인이 순회
+    		for(int i=0;i<N;i++) {
+    			if(M == 0) break;
+    			
+    			for(int j=crain_positions[i];j<box.length;j++) {
+    				if(visited[j] == true) continue; 
+    				if(crain[i] < box[j]) {
+    					crain_positions[i]++;
+    					continue;
+    				}
+    				else if(crain[i] >= box[j]) {
+    					visited[j] = true;
+    					M--;
+    					break;
+    				}
+    			}
+    			
+//                비효율적인 코드
+//    			for(int j=0;j<box.length;j++) {
+//    				if(visited[j] == true) continue; 
+//    				if(crain[i] >= box[j]) {
+//    					visited[j] = true;
+//    					M--;
+//    					break;
+//    				}
+//    			}
+    			
+    		}
+    		answer++;
+    	}
+    	System.out.println(answer);
+    	
     }
 }
