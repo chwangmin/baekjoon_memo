ac39440f85965a33c7f4be8d78f12ed4e69d7aa1
chwangmin <44563065+chwangmin@users.noreply.github.com>
2024-03-11 22:26:20 +0900

    [Gold V] Title: 배, Time: 196 ms, Memory: 15440 KB -BaekjoonHub

--- 백준/Gold/1092. 배/배.java
+++ 백준/Gold/1092. 배/배.java
@@ -1,83 +1,90 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
- 
- 
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
 public class Main {
-	
-	public static int N,M;
-	public static Integer[] crain, box;
-	public static int answer =0 ;
-	public static boolean[] visited;
-	public static int[] crain_positions;
-    public static void main(String[] args) throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	
-    	StringTokenizer st = new StringTokenizer(br.readLine());
-    	N = Integer.parseInt(st.nextToken());
-    	crain = new Integer[N];
-    	
-    	st = new StringTokenizer(br.readLine());
-    	for(int i=0;i<N;i++) {
-    		crain[i] = Integer.parseInt(st.nextToken());
-    	}
- 
-    	st = new StringTokenizer(br.readLine());
-    	M = Integer.parseInt(st.nextToken());
-    	box = new Integer[M];
-    	
-    	st = new StringTokenizer(br.readLine());
-    	for(int i=0;i<M;i++) {
-    		box[i] = Integer.parseInt(st.nextToken());
-    	}
-    	
-    	//내림차순으로 정렬하여야, 큰것들부터 처리합니다. (큰 크레인이 작은것들을 처리하는 일이 없어야만 합니다.)
-    	Arrays.sort(crain, Collections.reverseOrder());
-    	Arrays.sort(box, Collections.reverseOrder());
-    	
-    	
-    	if(crain[0] < box[0]) {
-    		System.out.println("-1"); 
-    		return;
-    	}
- 
-    	crain_positions = new int[N];
-    	visited = new boolean[M];
-    	//박스를 모두 옮기기전까지 작동
-    	while(M > 0) {
-    		
-    		//각 크레인이 순회
-    		for(int i=0;i<N;i++) {
-    			if(M == 0) break;
-    			
-    			for(int j=crain_positions[i];j<box.length;j++) {
-    				if(visited[j] == true) continue; 
-    				if(crain[i] < box[j]) {
-    					crain_positions[i]++;
-    					continue;
-    				}
-    				else if(crain[i] >= box[j]) {
-    					visited[j] = true;
-    					M--;
-    					break;
-    				}
-    			}
-    			
-//                비효율적인 코드
-//    			for(int j=0;j<box.length;j++) {
-//    				if(visited[j] == true) continue; 
-//    				if(crain[i] >= box[j]) {
-//    					visited[j] = true;
-//    					M--;
-//    					break;
-//    				}
-//    			}
-    			
-    		}
-    		answer++;
-    	}
-    	System.out.println(answer);
-    	
-    }
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		//crane 무게 제한 입력
+		List<Integer> crane = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			crane.add(Integer.parseInt(st.nextToken()));
+		}
+		//box 무게 입력
+		int M = Integer.parseInt(br.readLine());
+		List<Integer> box = new ArrayList<>();
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<M; i++) {
+			box.add(Integer.parseInt(st.nextToken()));
+		}
+		//역순 정렬
+		Collections.sort(crane, Comparator.reverseOrder());
+		Collections.sort(box, Comparator.reverseOrder());
+		//변수 선언
+		//time -> 몇분안에 박스를 옮겼는지
+		int time = 0;
+		//cnt -> 현재까지 옮긴 박스의 갯수
+		int cnt = 0;
+		//ischecked -> 해당 박스가 옮겨졌는지 확인
+		boolean[] isChecked = new boolean[M];
+		//craneVisitIdx -> 해당 크레인이 이전에 옮긴 박스의 index
+		int[] craneVisitIdx = new int[N];
+		
+		//만약, 가장 무거운 무게가 크래인이 들수 있는 무게보다 무겁다면 -1출력
+		if(box.get(0) > crane.get(0)) {
+			System.out.println(-1);
+		}
+		//그외의 경우
+		else {
+			//모든 박스를 옮길때까지 반복하면서
+			while(cnt<M) {
+				//시간을 +1만큼 해주고
+				time++;
+				//각 크래인별로 반복하면서
+				for(int i=0; i<N; i++) {
+					//만약, 크레인이 이전에 아무곳도 반복하지 못했다면.. 이후에도 반복하지 못할것이기 때문에 continue
+					if(craneVisitIdx[i]==-1) {
+						continue;
+					}
+					//크레인이 이전에 방문했던곳부터 방문하며(원래 +1 로직이 조금 더 맞긴한데 0일때 처리가 힘듬)
+					for(int j=craneVisitIdx[i]; j<M; j++) {
+						//만약, 현재 box가 방문되지 않았고 해당 크레인이 상자의 무게를 옮길수 있다면
+						 if(!isChecked[j] && (crane.get(i) >= box.get(j))) {
+							 //방문 여부 변경
+							 isChecked[j] = true;
+							 //갯수 +1
+							 cnt++;
+							 //crane이 방문한 index변경
+							 craneVisitIdx[i] = j;
+							 //자신의 해당 차례 종료
+							 break;
+						 }
+						 //만약, 무게를 옮기지 못하고 마지막 상자까지 왔다면
+						 if(j==M-1) {
+							 //방문을 못했다는 flag를 -1로 선언
+							 craneVisitIdx[i] = -1;
+						 }
+					}
+					//만약, 다 옮겼다면 정지
+					if(cnt==M) {
+						break;
+					}
+				}
+				
+			}
+			System.out.println(time);
+		}
+	}
 }
