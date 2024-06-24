b316ba3eecd07ff77c1146e645703209ad064c96
Choi GwangMin <ckm7907@naver.com>
2024-06-25 02:46:12 +0900

    [Silver I] Title: NM과 K (1), Time: 192 ms, Memory: 13420 KB -BaekjoonHub

--- 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
+++ 백준/Silver/18290. NM과 K （1）/NM과 K （1）.java
@@ -1,68 +1,78 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M, K, answer;
-    static int[][] arr;
+
+    static int N, M, K;
+
+    static int[][] map;
+
     static boolean[][] visited;
-    static int[] dx = {-1, 0, 1, 0};
-    static int[] dy = {0, -1, 0, 1};
 
-    public static void solve(int x, int y, int idx, int sum) {
-        if (idx == K) {
-            answer = Math.max(answer, sum);
-            return;
-        } else {
-            for (int i = x; i < N; i++) {
-                for (int j = y; j < M; j++) {
-                    if (!visited[i][j]) {
-                        if (check(i, j)) {
-                            visited[i][j] = true;
-                            solve(x, y, idx + 1, sum + arr[i][j]);
-                            visited[i][j] = false;
-                        }
-                    }
-                }
-            }
-        }
-
-    }
-
-    public static boolean check(int x, int y) {
-        boolean flag = true;
-        for (int i = 0; i < 4; i++) {
-            int next_x = x + dx[i];
-            int next_y = y + dy[i];
-
-            if (next_x >= 0 && next_x < N && next_y >= 0 && next_y < M) {
-                if (visited[next_x][next_y]) {
-                    flag = false;
-                }
-            }
-        }
-        return flag;
-    }
+    static int answer;
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
-        visited = new boolean[N + 1][M + 1];
-        answer = Integer.MIN_VALUE;
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st = new StringTokenizer(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = Integer.parseInt(st.nextToken());
+       N = Integer.parseInt(st.nextToken());
+       M = Integer.parseInt(st.nextToken());
+       K = Integer.parseInt(st.nextToken());
+
+       map = new int[N][M];
+       visited = new boolean[N][M];
+
+       answer = Integer.MIN_VALUE;
+
+       for (int i = 0; i < N; i++) {
+           st = new StringTokenizer(br.readLine());
+           for (int j = 0; j < M; j++) {
+               map[i][j] = Integer.parseInt(st.nextToken());
+           }
+       }
+
+       solve(0,0,0,0);
+       System.out.println(answer);
+    }
+
+    private static void solve(int r, int c, int idx, int sum) {
+        if(idx == K){
+            answer = Math.max(answer, sum);
+            return;
+        }
+        for (int i = r; i < N; i++) {
+            for (int j = (i == r ? c : 0); j < M; j++) {
+                if(visited[i][j]){
+                    continue;
+                }
+                if(check(i,j)) {
+                    visited[i][j] = true;
+                    solve(i, 0, idx+1, sum + map[i][j]);
+                    visited[i][j] = false;
+                }
             }
         }
+    }
 
-        solve(0, 0, 0, 0);
-        System.out.println(answer);
+
+    static int[] dr = {1,0,0,-1};
+    static int[] dc = {0,-1,1,0};
+
+    private static boolean check(int r, int c) {
+        for (int i = 0; i < 4; i++) {
+            int nr = r + dr[i];
+            int nc = c + dc[i];
+
+            if(nr < 0 || nr >= N || nc < 0 || nc >= M){
+                continue;
+            }
+            if (visited[nr][nc]) {
+                return false;
+            }
+        }
+        return true;
     }
 }
